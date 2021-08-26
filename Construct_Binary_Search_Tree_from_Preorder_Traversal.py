92beab369a15e693318bba9aa8b7fe8d6f68d8e4
saurabh jain <sjbpr1@gmail.com>
2021-08-26 22:54:08 +0530

    Create Construct_Binary_Search_Tree_from_Preorder_Traversal.py

---
+++ Construct_Binary_Search_Tree_from_Preorder_Traversal.py
@@ -0,0 +1,65 @@
+'''Construct Binary Search Tree from Preorder Traversal
+Link:- https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+
+
+question:- 
+
+Given an array of integers preorder, which represents the preorder traversal of a BST (i.e., binary search tree), construct the tree and return its root.
+
+It is guaranteed that there is always possible to find a binary search tree with the given requirements for the given test cases.
+
+A binary search tree is a binary tree where for every node, any descendant of Node.left has a value strictly less than Node.val, and any descendant of Node.right has a value strictly greater than Node.val.
+
+A preorder traversal of a binary tree displays the value of the node first, then traverses Node.left, then traverses Node.right.
+
+ 
+
+Example 1:
+
+
+Input: preorder = [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+Example 2:
+
+Input: preorder = [1,3]
+Output: [1,null,3]
+ 
+
+Constraints:
+
+1 <= preorder.length <= 100
+1 <= preorder[i] <= 108
+All the values of preorder are unique.
+
+
+
+
+'''
+####Solution:- 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        root=TreeNode(preorder[0])
+        for i in range(1,len(preorder)):
+            x=root
+            y=root
+            temp=TreeNode(preorder[i])
+            while x is not None:
+                if preorder[i]>x.val:
+                    y=x
+                    x=x.right
+                else:
+                    y=x
+                    x=x.left
+            if y.val>preorder[i] :
+                y.left=temp
+            else:
+                y.right=temp
+        return root        
